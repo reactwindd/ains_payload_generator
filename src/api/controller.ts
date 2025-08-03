2a51b0e86174f3de48dde6cb82963ecb34b5a8a4
reactwindd <71582030+reactwindd@users.noreply.github.com>
2025-08-03 20:48:25 +0800

    Update controller.ts

--- src/api/controller.ts
+++ src/api/controller.ts
@@ -121,43 +121,6 @@ export async function getBook() {
         book.items[0].volumeInfo.publishedDate = "-";
     }
 
-    // **********************************************************
-    //
-    // Only For Debugging Purposes
-    //
-    // **********************************************************
-    // console.log({
-    //     data: {
-    //         user: userData.id,
-    //         type: "book",
-    //         date: formatDate(Date.now()),
-    //         title: book.items[0].volumeInfo.title,
-    //         bookType: "physical",
-    //         category: "fiction",
-    //         noOfPage: book.items[0].volumeInfo.pageCount
-    //             ? book.items[0].volumeInfo.pageCount
-    //             : 0,
-    //         isbn: book.items[0].volumeInfo.industryIdentifiers
-    //             ? book.items[0].volumeInfo.industryIdentifiers[0].identifier
-    //             : "-",
-    //         author: book.items[0].volumeInfo.authors
-    //             ? book.items[0].volumeInfo.authors[0]
-    //             : "-",
-    //         publisher: book.items[0].volumeInfo.publisher
-    //             ? book.items[0].volumeInfo.publisher
-    //             : "-",
-    //         publishedYear: formatPublishedDate(
-    //             book.items[0].volumeInfo.publishedDate
-    //         ),
-    //         language: "en",
-    //         summary: book.items[0].volumeInfo.description
-    //             ? book.items[0].volumeInfo.description
-    //             : "No Description",
-    //         review: "It's Really Good",
-    //         rating: 5,
-    //         reviewIsVideo: false,
-    //     },
-    // });
     const author = book.items[0].volumeInfo.authors
         ? book.items[0].volumeInfo.authors[0]
         : "-";
@@ -204,77 +167,159 @@ export async function getBook() {
 
 // ***************************************************************
 //
-// api/insertRecord
+// api/getbookpri
 //
 // ***************************************************************
+export async function deepReviewPri(
+    title: string,
+    publishedYear: string,
+    author: string
+) {
+    const openai = new OpenAI({
+        baseURL: "https://api.deepseek.com",
+        apiKey: process.env.DEEPSEEK_API_KEY,
+    });
 
-// export async function insertRecord(token: string): Promise<book> {
-//     const user = await fetch("https://jombaca-api.jazro.com.my/api/users/me", {
-//         method: "GET",
-//         headers: {
-//             Authorization: `Bearer ${token}`,
-//             Origin: "https://ains.moe.gov.my",
-//         },
-//     });
+    const completion = await openai.chat.completions.create({
+        messages: [
+            {
+                role: "system",
+                content: `
+Write a 15-word review of "Never Trumpet with a Crumpet" published 2019 by Amy Gibson in perspective of a primary school learner non native speaker tone. Use only:
+- Letters, commas, periods, and basic punctuation
+- No line breaks (\n), asterisks, or special formatting
+- Exactly 15 words
+- Simple English words (A0 level)`,
+            },
+        ],
+        model: "deepseek-chat",
+        response_format: {
+            type: "text",
+        },
+    });
 
-// const book = await getBook(token);
-// console.log(book);
+    let result = completion.choices[0].message.content;
+    result = result.replace(/\\|\*|_/g, "");
 
-// let bookData = book;
-// let userData = await user.json();
-// console.log(bookData);
+    return result;
+}
 
-// let body = JSON.stringify({
-//     data: {
-//         user: userData.id,
-//         type: "book",
-//         date: bookData.data.date,
-//         title: bookData.data.title,
-//         bookType: "physical",
-//         category: "fiction",
-//         noOfPage: bookData.data.noOfPage,
-//         isbn: bookData.data.isbn,
-//         author: bookData.data.author,
-//         publisher: bookData.data.publisher,
-//         publishedYear: bookData.data.publishedYear,
-//         language: "en",
-//         summary: bookData.data.summary,
-//         review: "It's Really Good",
-//         rating: 5,
-//         reviewIsVideo: false,
-//     },
-// });
+export async function deepSumPri(
+    title: string,
+    publishedYear: string,
+    author: string
+) {
+    const openai = new OpenAI({
+        baseURL: "https://api.deepseek.com",
+        apiKey: process.env.DEEPSEEK_API_KEY,
+    });
 
-//     const data = await fetch(
-//         "https://jombaca-api.jazro.com.my/api/nilam-records",
-//         {
-//             method: "POST",
-//             headers: {
-//                 Authorization: `Bearer ${token}`,
-//                 Origin: "https://ains.moe.gov.my",
-//                 "Content-Type": "application/json",
-//                 Accept: "application/json, text/plain, */*",
-//             },
-//             // body: body,
-//         }
-//     );
+    const completion = await openai.chat.completions.create({
+        messages: [
+            {
+                role: "system",
+                content: `
+Write a 15-word summary of "Never Trumpet with a Crumpet" published 2019 by Amy Gibson in perspective of a primary school learner non native speaker tone. Use only:
+- Letters, commas, periods, and basic punctuation
+- No line breaks (\n), asterisks, or special formatting
+- Exactly 15 words
+- Simple English words (A0 level)`,
+            },
+        ],
+        model: "deepseek-chat",
+        response_format: {
+            type: "text",
+        },
+    });
 
-//     let result = await data.json();
+    let result = completion.choices[0].message.content;
+    result = result.replace(/\\|\*|_/g, "");
 
-//     return result;
-// }
+    return result;
+}
 
-// export async function findPerson(token: string) {
-//     const data = await fetch(
-//         "https://jombaca-api.jazro.com.my/api/nilam-records?sort[-1]=createdAt&populate=*",
-//         {
-//             method: "GET",
-//             headers: {
-//                 Authorization: `Bearer ${token}`,
-//                 Origin: "https://ains.moe.gov.my",
-//             },
-//         }
-//     );
-//     const result = await data.json();
-//     return result;
-// }
+export async function getBookPri() {
+    const word = await fetch("https://random-word-api.vercel.app/api?words=1");
+    const wordDataa = await word.json();
+    const wordData = wordDataa[0];
+    const data = await fetch(
+        `https://www.googleapis.com/books/v1/volumes/?q=${await wordData}`
+    );
+    // const user = await fetch("https://jombaca-api.jazro.com.my/api/users/me", {
+    //     method: "GET",
+    //     headers: {
+    //         Authorization: `Bearer ${token}`,
+    //         Origin: "https://ains.moe.gov.my",
+    //     },
+    // });
+
+    let book = await data.json();
+    // let userData = await user.json();
+
+    function formatDate(date: number) {
+        var d = new Date(date),
+            month = "" + (d.getMonth() + 1),
+            day = "" + d.getDate(),
+            year = d.getFullYear();
+
+        if (month.length < 2) month = "0" + month;
+        if (day.length < 2) day = "0" + day;
+
+        return [year, month, day].join("-");
+    }
+
+    function formatPublishedDate(date: string) {
+        return date.slice(0, 4);
+    }
+
+    if (!book.items[0].volumeInfo.publishedDate) {
+        book.items[0].volumeInfo.publishedDate = "-";
+    }
+
+    const author = book.items[0].volumeInfo.authors
+        ? book.items[0].volumeInfo.authors[0]
+        : "-";
+
+    const review = await deepReviewPri(
+        book.items[0].volumeInfo.title,
+        formatPublishedDate(book.items[0].volumeInfo.publishedDate),
+        author
+    );
+
+    const summary = await deepSumPri(
+        book.items[0].volumeInfo.title,
+        formatPublishedDate(book.items[0].volumeInfo.publishedDate),
+        author
+    );
+
+    return {
+        data: {
+            user: "NaN",
+            type: "book",
+            date: formatDate(Date.now()),
+            title: book.items[0].volumeInfo.title,
+            bookType: "physical",
+            category: "fiction",
+            noOfPage: book.items[0].volumeInfo.pageCount
+                ? book.items[0].volumeInfo.pageCount
+                : 0,
+            isbn: book.items[0].volumeInfo.industryIdentifiers
+                ? book.items[0].volumeInfo.industryIdentifiers[0].identifier
+                : "-",
+            author: book.items[0].volumeInfo.authors
+                ? book.items[0].volumeInfo.authors[0]
+                : "-",
+            publisher: book.items[0].volumeInfo.publisher
+                ? book.items[0].volumeInfo.publisher
+                : "-",
+            publishedYear: formatPublishedDate(
+                book.items[0].volumeInfo.publishedDate
+            ),
+            language: "en",
+            summary: summary,
+            review: review,
+            rating: 5,
+            reviewIsVideo: false,
+        },
+    };
+}
